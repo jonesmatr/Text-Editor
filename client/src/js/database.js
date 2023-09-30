09c4bb76ba600d428b8c41873b8d3ec3829bfc48
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-09-30 11:07:20 -0400

    Fixed typo in database.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -18,7 +18,7 @@ export const putDb = async (content) => {
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   const request = store.add({ content });
-  const requests = await request;
+  const result = await request;
   console.log('Content added to the database', result);
 };
 
