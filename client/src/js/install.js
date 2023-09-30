1d1f8a3db32f35d526a0ff58ce8d9683f7c49544
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-09-30 10:41:33 -0400

    Updated event handlers

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,43 +1,38 @@
 const butInstall = document.getElementById('buttonInstall');
 const textHeader = document.getElementById('textHeader');
 
+
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
-    installBtn.style.visibility = 'visible';
+    butInstall.style.visibility = 'visible';
     textHeader.textContent = 'Click the button to install!';
 
-    installBtn.addEventListener('click', () => {
+    butInstall.addEventListener('click', () => {
         event.prompt();
-        installBtn.setAttribute('disabled', true);
-        installBtn.textContent = 'Installed!';
+        butInstall.setAttribute('disabled', true);
+        butInstall.textContent = 'Installed!';
     });
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const promptEvent = window.deferredPrompt;
 
-  if (!promptEvent) {
-   return;
-  }
+    if (!promptEvent) {
+        return;
+    }
 
-  // Show prompt
-  promptEvent.prompt();
-  
-  // Reset the deferred prompt variable, it can only be used once.
-  window.deferredPrompt = null;
-  
-  butInstall.classList.toggle('hidden', true);
+    // Show the install prompt
+    promptEvent.prompt();
 
+    // Reset the deferred prompt variable, it can only be used once
+    window.deferredPrompt = null;
 
-
-    // Logic to install the app goes here
-    // The 'beforeinstallprompt' event handler above should actually handle the installation process
+    // Hide the install button after user's response
+    butInstall.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {
-    window.deferredPrompt = null;
+window.addEventListener('appinstalled', (evt) => {
+    console.log('The app was installed successfully!');
+    butInstall.style.display = 'none'; // You can hide or disable the install button here
 });
